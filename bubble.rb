4460b7c35ea99ff3bd21b1603ae7c978eefc341f
mohammadumar28 <mohammadumar28@gmail.com>
2020-07-15 19:13:20 +0530

    Bubble Sort method implemented

--- bubble.rb
+++ bubble.rb
@@ -1,1 +1,18 @@
-puts 'Hello, World!'
+def bubble_sort(arr)
+  if arr.length == 1
+    puts 'Array has been sorted!'
+  else
+    x = 0
+    until x == (arr.length - 1) do
+      for i in 1..arr.length-1
+        if arr[i-1] > arr[i]
+          arr[i-1], arr[i] = arr[i], arr[i-1]
+        end
+      end
+      x += 1
+    end
+  end
+  return arr
+end
+
+bubble_sort([12, 1, 4, 5, 6, 0, 11, 9])
